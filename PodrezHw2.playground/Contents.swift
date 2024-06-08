aa168ce0611c55f36cef88644604925cb12c2ad8
PodrezOleg <bolynita@icloud.com>
2024-06-09 00:12:32 +0300

    Начало домашнего задания 2 лекцкии (Констнты, Типы, Оператор)

---
+++ PodrezHw2.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+//1. Создать новый Playground
+
+var greeting = "Hello, playground"
+
+//2. Написать переменные и константы всех базовых типов данных: Int, Bool, Float,Double, String.
+
+var x: Int = 14062024
+let xx: Float  = 2.123455
+var xyx: Double = 4.599398530958930
+var xyy: Bool = true
+var yyy: String = "Mars"
+
+//2.1 У чисел вывести их минимальные и максимальные значения (Int8/16…UInt…), а у строки – количество символов.
+
+print (Int.max); (Int.min)
+print (Int8.max); (Int8.min)
+print (UInt8.max); (UInt8.min)
+print (UInt16.max); (UInt16.min)
+
+//2.1.1 КОличество символов
+
